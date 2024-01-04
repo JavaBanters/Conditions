3011723162257e90fb46e2d80f9df58bb5c20255
Anant Majhi <anmajhi7@gmail.com>
2024-01-04 11:08:52 +0530

    condition logic added

---
+++ src/example1/Conditions.java
@@ -0,0 +1,71 @@
+package example1;
+
+
+//    if (condition) {
+//        //
+//    } else {
+//
+//    }
+
+    /*if (condition) {
+
+    } else if (another condition) {
+
+    } else if (condition) {
+
+    } else {
+
+    }*/
+
+  /*if() {
+            if() {
+
+            }else () {
+
+            }
+        } else {
+
+      }*/
+
+ // == comparison
+ // && all the conditions should hold true
+ // || either one condition should be true
+public class Conditions {
+
+
+    public static void main(String[] args) {
+
+//        int a = 11;
+//        String name = "Anant";
+//
+//        if ( name.equals("Anant")) { //== dont compare value of string with == operator
+//            System.out.println("It is true");
+//        } else {
+//            System.out.println("It is false");
+//        }
+
+//        int science = 30;
+//        int maths = 30;
+//
+//        if (maths > 40 || science > 40 ) {
+//            System.out.println("I will buy you a bicycle");
+//        } else {
+//            System.out.println("i wont' buy anything ");
+//        }
+
+//        int a = 10;
+//
+//        if ( a != 10) {
+//            System.out.println("It is true");
+//        } else {
+//            System.out.println("It is false");
+//        }
+
+
+
+
+    }
+
+
+
+}
