5d0c05fd1d19c40a2349ff08a34722a0e90d3416
Anant Majhi <anmajhi7@gmail.com>
2024-01-09 10:03:28 +0530

    more array examples

---
+++ src/example7/ArrayExample1.java
@@ -0,0 +1,26 @@
+package example7;
+
+public class ArrayExample1 {
+
+    public static void main(String[] args) {
+        int arr [] = {43, 29, 21, 67, 29 ,1 ,29, 34, 78, 21};
+        int target = 29;
+        int result = findOccurrences(arr, target);
+        System.out.println("The occurrence of the target "+target+ " is "+result);
+    }
+
+
+    static int findOccurrences(int arr[], int target) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+
+}
---
+++ src/example7/ArrayExample4.java
@@ -0,0 +1,9 @@
+package example7;
+
+public class ArrayExample4 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
